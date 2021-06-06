d42633f031714ddd9e869954a63f0a39a1762126
weibo <919006380@qq.com>
2021-06-06 13:20:04 +0800

    add

--- www/src/main.js
+++ www/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from './router'
-import api from './api';
+import {plugin} from './api';
+import 'babel-polyfill'
 
 const app = createApp(App)
 app.use(router)
-app.use(api)
+app.use(plugin)
 app.mount('#app')
 
--- www/src/views/list.vue
+++ www/src/views/list.vue
@@ -1,13 +1,21 @@
 <template>
-    <button @click="on"></button>
+    <button @click="post">asd</button>
+
+    <button @click="xxx">test</button>
 </template>
 <script>
 export default {
-    setup() {
-        function on() {
-            console.log(123);
-        }
-        return { on }
+    methods: {
+        async xxx() {
+            await this.$api.blog.detail({ a: "s" })
+        },
+        async post() {
+            var { data } = await this.$api.blog.detail({
+                title: "xxx",
+                content: "内容1020",
+            });
+            console.log(data);
+        },
     }
 }
 </script>
